c0f9e2cf8cc16dcfbae0606bf2d44398be3ddfdd
Maksym <144346873+makseniuk@users.noreply.github.com>
2024-08-04 12:04:05 +0200

    0.0.9

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,12 +1,13 @@
-import { Contact } from "../db/Models/contactsModel.js";
+import { Contact } from "../db/Models/contactsModel,js";
 
 const getAllContacts = async () => {
-  try {
-    const contacts = await Contact.find({});
-    return contacts;
-  } catch (error) {
-    throw new Error('Failed to fetch contacts: ' + error.message);
-  }
+  const contacts = await Contact.find({});
+  return contacts;
 };
 
-export { getAllContacts };
+const getContactById = async (contactId) => {
+  const contact = await Contact.findById(contactId);
+  return contact;
+};
+
+export { getAllContacts, getContactById };
