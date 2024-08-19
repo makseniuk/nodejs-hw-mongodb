3fe8fdd81f3ab5bd000b66347c8f8e10baa4c532
Maksym <144346873+makseniuk@users.noreply.github.com>
2024-08-19 13:49:01 +0200

    0.0.1

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { registerUser, loginUser, refreshSession, logoutUser } from '../controllers/auth.js';
+import { registerUser, loginUser, refreshSession, logoutUser, sendResetEmail } from '../controllers/auth.js';
 import validateBody from '../middlewares/validateBody.js';
 import { userRegisterSchema, userLoginSchema } from '../validation/authValidation.js';
+import { resetEmailSchema } from '../validation/authValidation.js';
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.post('/register', validateBody(userRegisterSchema), registerUser);
 router.post('/login', validateBody(userLoginSchema), loginUser);
 router.post('/refresh', refreshSession);
 router.post('/logout', logoutUser);
+router.post('/send-reset-email', validateBody(resetEmailSchema), sendResetEmail);
+
 
 export default router;
