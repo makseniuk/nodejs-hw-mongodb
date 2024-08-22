83edb5d44e05d994b45289d9988b47facdcc723e
Maksym <144346873+makseniuk@users.noreply.github.com>
2024-08-22 10:50:04 +0200

    0.0.2

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,4 +7,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads',
+    format: async (req, file) => 'png',
+    public_id: (req, file) => file.originalname.split('.')[0],
+  },
+});
+
+export { cloudinary, storage };
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-import { storage } from '../config/cloudinary.js'; // Імпортуйте налаштування Cloudinary
+import { storage } from '../config/cloudinary.js';
 
 const upload = multer({ storage });
 
